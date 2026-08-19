ef84a093a90b4ec82de49d6836430eeb36f57637
tanish418 <tanishyt4181@gmail.com>
2026-08-19 15:23:36 +0530

    Add: sign-of-the-product-of-an-array - submission-3

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        negative = 0
+
+        for num in nums:
+            if num == 0:
+                return 0
+            if num < 0:
+                negative += 1
+
+        return -1 if negative % 2 else 1
