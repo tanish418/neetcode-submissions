4fb98219321e013ee7fc621e11a63151e185926c
tanish418 <tanishyt4181@gmail.com>
2026-08-23 07:48:21 +0530

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for c in logs:
+            if c =="../":
+                count-=1
+            elif c=="./":
+                count+=0
+            else:
+                count+=1
+        return count                
